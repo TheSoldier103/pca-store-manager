bb5e24fb85e3128e2a65a70d04cf39816b96a0fa
Ufuoma.Apoki <ufuomaapoki@gmail.com>
2026-05-22 09:53:39 +0300

    Initial Setup

---
+++ pca-store-manager.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Plugin Name: PCA Store Manager
+ * Description: Multi-campus bookshop and uniform manager for PCA.
+ * Version: 0.1.0
+ * Author: PCA
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+define( 'PCA_STORE_VERSION', '0.1.0' );
+define( 'PCA_STORE_PATH', plugin_dir_path( __FILE__ ) );
+define( 'PCA_STORE_URL', plugin_dir_url( __FILE__ ) );
+
+require_once PCA_STORE_PATH . 'includes/class-activator.php';
+require_once PCA_STORE_PATH . 'includes/class-permissions.php';
+require_once PCA_STORE_PATH . 'admin/class-admin-menu.php';
+
+register_activation_hook( __FILE__, [ 'PCA_Store_Activator', 'activate' ] );
+
+add_action( 'plugins_loaded', function() {
+    PCA_Store_Permissions::init();
+    PCA_Store_Admin_Menu::init();
+});
