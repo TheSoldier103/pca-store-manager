8ec599fd0f24fcada40b3371a1da4817ab81a092
Ufuoma.Apoki <ufuomaapoki@gmail.com>
2026-05-23 15:22:43 +0300

    Wiring suppliers

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,21 +1,21 @@
-jQuery(document).ready(function($){
+// jQuery(document).ready(function($){
 
-    // Open modal
-    $('#pca-add-stationery-btn').on('click', function(){
-        $('#pca-add-stationery-modal').show();
-    });
+//     // Open modal
+//     $('#pca-add-stationery-btn').on('click', function(){
+//         $('#pca-add-stationery-modal').show();
+//     });
 
-    // Close modal
-    $('#pca-close-stationery-modal').on('click', function(){
-        $('#pca-add-stationery-modal').hide();
-    });
+//     // Close modal
+//     $('#pca-close-stationery-modal').on('click', function(){
+//         $('#pca-add-stationery-modal').hide();
+//     });
 
-    // Save (backend will be added later)
-    $('#pca-save-stationery').on('click', function(){
-        alert('Saving stationery... (backend coming later)');
-    });
+//     // Save (backend will be added later)
+//     $('#pca-save-stationery').on('click', function(){
+//         alert('Saving stationery... (backend coming later)');
+//     });
 
-});
+// });
 
 jQuery(document).ready(function($){
 
