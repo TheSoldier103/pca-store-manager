336f5c6be50bd1556007475d007d6845ad7d8860
Ufuoma.Apoki <ufuomaapoki@gmail.com>
2026-05-23 08:52:50 +0300

    Wiring Settings - Schools

--- pca-store-manager.php
+++ pca-store-manager.php
@@ -51,10 +51,18 @@ add_action('plugins_loaded', function () {
 | This is the FIX. No hooks. No timing issues.
 |--------------------------------------------------------------------------
 */
-PCA_Store_Settings_Controller::init();
-PCA_Store_Items_Controller::init();
-PCA_Store_Stock_Controller::init();
-PCA_Store_Reports_Controller::init();
+// PCA_Store_Settings_Controller::init();
+// PCA_Store_Items_Controller::init();
+// PCA_Store_Stock_Controller::init();
+// PCA_Store_Reports_Controller::init();
+
+add_action('init', function () {
+
+    error_log('MAIN INIT FIRED');
+
+    PCA_Store_Settings_Controller::init();
+
+});
 
 /*
 |--------------------------------------------------------------------------
