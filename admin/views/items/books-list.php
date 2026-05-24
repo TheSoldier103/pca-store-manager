5b3ba7739be53cd0b1dcbaf891ff4867181b0dc0
Ufuoma.Apoki <ufuomaapoki@gmail.com>
2026-05-24 12:12:49 +0300

    Updating Items Packs

--- admin/views/items/books-list.php
+++ admin/views/items/books-list.php
@@ -47,7 +47,7 @@
                 echo '<td>' . intval($book->current_stock) . '</td>';
                 echo '<td>' . esc_html($book->status) . '</td>';
                 echo '<td style="white-space: nowrap;">';
-                echo '<a href="#" class="pca-edit-item" data-id="' . $book->id . '">Edit</a> ';
+                echo '<a href="#" class="pca-edit-item" data-id="' . $book->id . '">Edit</a> ' | ;
                 echo '<a href="#" class="button-danger pca-delete-item" data-id="' . $book->id . '">Delete</a>';
                 
 
